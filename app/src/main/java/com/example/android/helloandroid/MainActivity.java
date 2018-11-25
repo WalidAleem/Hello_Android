bf630fc304406e54d98449cc287432704934bd4b
Walid AbdulAleem <walidaaleem@gmail.com>
2018-11-25 14:07:38 +0200

    Initial Commit - Creating repository

---
+++ app/src/main/java/com/example/android/helloandroid/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.android.helloandroid;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+        }
+
+
+    }
+
+
+
+
+
+
